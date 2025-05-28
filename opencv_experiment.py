20f4f63d78c94cb03fd961fe883991bf412c40f9
Daan Ypma <daan.ypma@gmail.com>
2025-05-28 13:52:11 +0200

    Iadded the function that will put the 2d image to 3d points, however the Q parameter needs to be loaded or calculated, I added a link to the doc in the file.

--- opencv_experiment.py
+++ opencv_experiment.py
@@ -1,4 +1,3 @@
-from coppeliasim_zmqremoteapi_client import *
 import numpy as np
 import cv2 as cv
 
@@ -18,19 +17,27 @@ def filterTable(img):
     result = cv.bitwise_and(img,img, mask=mask)
     return result
 
+def disparityToPointCloud(disparity, Q):
+    # Reproject disparity to 3D space
+    points_3D = cv.reprojectImageTo3D(disparity, Q)
+    return points_3D
 
 # ----------------- Main ----------------- #
 client = RemoteAPIClient()
 sim = client.require('sim')
 sim.startSimulation()
 
+
 # Get vision sensor handle
 visionSensor = sim.getObject('./visionSensor')
 print(f'visionSensor handle : {visionSensor}')
 
+disparity = stereo.compute(left_img, right_img).astype(np.float32) / 16.0  # Create disparity map
+Q = ...  # This needs to be loaded or calculated from stereo calibrationb:  https://docs.opencv.org/4.11.0/d9/d0c/group__calib3d.html#ga617b1685d4059c6040827800e72ad2b6 
+points_3D = disparityToPointCloud(disparity, Q)
+
 while True:
     img = readFrame()
-    img = filterTable(img)    
+    img = filterTable(img)
     cv.imshow('img', img)
     cv.waitKey(1)
-    
