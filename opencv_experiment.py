48ca4e44038947b6a587b2df6a160ff469c33568
Zasasas20 <zaidassim2013@gmail.com>
2025-06-05 14:23:07 +0200

    Added disparity camera + small vision sensor adjustments

--- opencv_experiment.py
+++ opencv_experiment.py
@@ -9,7 +9,7 @@ def readFrame(visionObject):
     buf, res = sim.getVisionSensorImg(visionObject)
     img = np.frombuffer(buf, dtype=np.uint8).reshape(*res, 3)
     
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     return img
 
 # ----------------- Filter out background ---------------- #
@@ -38,15 +38,16 @@ visionSensorleft = sim.getObject('./visionSensorleft')
 print(f'visionSensor handle : {visionSensorleft}')
 print(f'visionSensor handle : {visionSensorright}')
 
-stereo = cv.StereoBM_create()
+stereo = cv.StereoBM_create(numDisparities=64,blockSize=15)
 
 #Q = ...  # This needs to be loaded or calculated from stereo calibrationb:  https://docs.opencv.org/4.11.0/d9/d0c/group__calib3d.html#ga617b1685d4059c6040827800e72ad2b6 
 #points_3D = disparityToPointCloud(disparity, Q)
 
 while True:
-    imgL = filterTable(readFrame(visionSensorleft))
-    imgR = filterTable(readFrame(visionSensorright))
-    #disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0  # Create disparity map
+    imgL = readFrame(visionSensorleft)
+    imgR = readFrame(visionSensorright)
+    disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0  # Create disparity map
     cv.imshow('imgL', imgL)
     cv.imshow('imgR', imgR)
+    cv.imshow('disparity', disparity)
     cv.waitKey(1)
