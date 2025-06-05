aeda6ccad3939933adef85cc786375238713727a
Daan Ypma <daan.ypma@gmail.com>
2025-06-05 11:35:36 +0200

    I added a second camera and alligned them so we have them perfectly alligned images

--- opencv_experiment.py
+++ opencv_experiment.py
@@ -1,11 +1,14 @@
+from coppeliasim_zmqremoteapi_client import *
 import numpy as np
 import cv2 as cv
 
+
 # ----------------- Read vision sensor ----------------- #
-def readFrame():
+def readFrame(visionObject):
     #sim.handleVisionSensor(visionSensor)
-    buf, res = sim.getVisionSensorImg(visionSensor)
+    buf, res = sim.getVisionSensorImg(visionObject)
     img = np.frombuffer(buf, dtype=np.uint8).reshape(*res, 3)
+    
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     return img
 
@@ -17,10 +20,10 @@ def filterTable(img):
     result = cv.bitwise_and(img,img, mask=mask)
     return result
 
-def disparityToPointCloud(disparity, Q):
+#def disparityToPointCloud(disparity, Q):
     # Reproject disparity to 3D space
-    points_3D = cv.reprojectImageTo3D(disparity, Q)
-    return points_3D
+ #   points_3D = cv.reprojectImageTo3D(disparity, Q)
+  #  return points_3D
 
 # ----------------- Main ----------------- #
 client = RemoteAPIClient()
@@ -29,15 +32,21 @@ sim.startSimulation()
 
 
 # Get vision sensor handle
-visionSensor = sim.getObject('./visionSensor')
-print(f'visionSensor handle : {visionSensor}')
+visionSensorright = sim.getObject('./visionSensorright')
+visionSensorleft = sim.getObject('./visionSensorleft')
 
-disparity = stereo.compute(left_img, right_img).astype(np.float32) / 16.0  # Create disparity map
-Q = ...  # This needs to be loaded or calculated from stereo calibrationb:  https://docs.opencv.org/4.11.0/d9/d0c/group__calib3d.html#ga617b1685d4059c6040827800e72ad2b6 
-points_3D = disparityToPointCloud(disparity, Q)
+print(f'visionSensor handle : {visionSensorleft}')
+print(f'visionSensor handle : {visionSensorright}')
+
+stereo = cv.StereoBM_create()
+
+#Q = ...  # This needs to be loaded or calculated from stereo calibrationb:  https://docs.opencv.org/4.11.0/d9/d0c/group__calib3d.html#ga617b1685d4059c6040827800e72ad2b6 
+#points_3D = disparityToPointCloud(disparity, Q)
 
 while True:
-    img = readFrame()
-    img = filterTable(img)
-    cv.imshow('img', img)
+    imgL = filterTable(readFrame(visionSensorleft))
+    imgR = filterTable(readFrame(visionSensorright))
+    #disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0  # Create disparity map
+    cv.imshow('imgL', imgL)
+    cv.imshow('imgR', imgR)
     cv.waitKey(1)
